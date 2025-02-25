9e147d9bcdd26e696eae4d0e97635ec97bbda81a
Obscura Student <service@obscuraworks.com>
2025-02-25 14:12:01 +0700

    Update smartsupp.js

--- api/smartsupp.js
+++ api/smartsupp.js
@@ -1,3 +1,11 @@
 module.exports = (req, res) => {
+  const allowedOrigin = "https://www.obscuraworks.com"; // Hanya domain ini yang boleh
+  const origin = req.headers.origin;
+
+  if (origin !== allowedOrigin) {
+    return res.status(403).json({ error: "Access Denied" }); // Blokir selain domain ini
+  }
+
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.status(200).json({ key: process.env.SMARTSUPP_KEY });
 };
